4c5b920aaf9e451aa0f1d37684df9acbd6d2e428
Marek.Waszczuk <seroocs@gmail.com>
2019-11-09 20:14:28 +0100

    Initial commit

---
+++ src/main/java/pl/solventive/LocalFarmer/LocalFarmerApi/LocalFarmerApiApplication.java
@@ -0,0 +1,13 @@
+package pl.solventive.LocalFarmer.LocalFarmerApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalFarmerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalFarmerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/solventive/LocalFarmer/LocalFarmerApi/LocalFarmerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.solventive.LocalFarmer.LocalFarmerApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalFarmerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
